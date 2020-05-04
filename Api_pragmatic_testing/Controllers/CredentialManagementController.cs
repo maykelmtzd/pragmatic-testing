7dff62fe70c246cccb7b9d621eea015d1247927e
Maykel <maykelmtzd@gmail.com>
2020-05-03 22:08:38 -0400

    Add first controller and router.

---
+++ Api_pragmatic_testing/Controllers/CredentialManagementController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_pragmatic_testing.Controllers
+{
+	[ApiController]
+	public class CredentialManagementController : ControllerBase
+	{
+
+		[HttpGet("api/users")]
+		public JsonResult GetUsers() 
+		{
+			return new JsonResult(
+					new List<object>()
+					{
+						new { Id = 1, Name = "Name1" },
+						new { Id = 2, Name = "Name2" }
+					}
+				);
+		}
+	}
+}
